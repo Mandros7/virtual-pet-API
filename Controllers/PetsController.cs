7fd4ff0abf62f14b0790ff7e0bf4f2e9862f274b
Hector Rodriguez <hectorrogue.9@gmail.com>
2020-07-04 14:06:03 +0200

    Initial commit with baseline structure

---
+++ Controllers/PetsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace mediatonic_pets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetController : ControllerBase
+    {
+        private readonly ILogger<PetController> _logger;
+
+        public PetController(ILogger<PetController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Pet> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Pet
+            {
+                lastUpdate = DateTime.Now.AddDays(-1*index),
+                hungriness = rng.Next(-100, 100),
+                happiness = rng.Next(-100, 100),
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Pets.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace mediatonic_pets
+{
+    public class Pet
+    {
+        public DateTime lastUpdate { get; set; }
+
+        public int happiness { get; set; }
+
+        public int hungriness { get; set; }
+
+    }
+}
